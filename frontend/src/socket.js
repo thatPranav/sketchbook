0811c1db3692730d717cea5722713c4f6e70dca9
thatPranav <jawajipranav@gmail.com>
2023-10-07 17:49:38 +0530

    feat: :arrow_up: endpoint by env

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,3 +1,6 @@
 import { io } from "socket.io-client";
-
-export const socket = io("http://localhost:5000");
+const url =
+  process.env.NODE_ENV === "production"
+    ? "https://sketchbook-server-dttl.onrender.com"
+    : "http://localhost:5000";
+export const socket = io(url);
